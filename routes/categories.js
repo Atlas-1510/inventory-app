992fbbc594ced13ac588112b84808005ed8fdf26
Jason Aravanis <j.aravanis@icloud.com>
2021-10-27 16:05:32 +1100

    Fix require error in categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const categoriesController = require("../controllers/categoriesController");
+const categoriesController = require("../controllers/categoryController");
 
 // GET categories page
 router.get("/", categoriesController.category_list);
