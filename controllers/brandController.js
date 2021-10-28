09aac135d1611d80e20cbf3b0cb755cdc77f1bd9
Jason Aravanis <j.aravanis@icloud.com>
2021-10-28 15:27:25 +1100

    Add form to create a new brand + express validation

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,6 +1,7 @@
 const async = require("async");
 const Brand = require("../models/brand");
 const Product = require("../models/product");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all brands
 exports.brand_list = function (req, res, next) {
@@ -40,13 +41,69 @@ exports.brand_detail = function (req, res, next) {
 
 // Display brand create form on GET
 exports.brand_create_get = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: Brand create form GET");
+  res.render("brand_form", { title: "Create Brand" });
 };
 
 // Handle brand create on POST
-exports.brand_create_post = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: Brand create form POST");
-};
+exports.brand_create_post = [
+  // Validate and sanitize the name field
+  body("title")
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .withMessage("Brand name must be within 1-100 characters")
+    .escape(),
+  // Validate and sanitize the description field.
+  body("description")
+    .trim()
+    .optional({ checkFalsy: true })
+    .isAlphanumeric()
+    .withMessage("Description can only contain alphanumeric characters.")
+    .escape(),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    // Create a brand object with escaped and trimmed data
+    const brand = new Brand({
+      title: req.body.title,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("brand_form", {
+        title: "Create Brand",
+        brand: brand,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from the form is valid
+      // Check if brand with same name already exists in database.
+      Brand.findOne({ title: req.body.title }).exec(function (
+        err,
+        found_brand
+      ) {
+        if (err) {
+          return next(err);
+        }
+        if (found_brand) {
+          // Brand already exists, redirect to brand page
+          res.redirect(found_brand.url);
+        } else {
+          brand.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            // New brand saved, redirect to brand detail page
+            res.redirect(brand.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 // Display brand delete form on GET
 exports.brand_delete_get = function (req, res, next) {
